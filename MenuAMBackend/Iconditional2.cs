8df84b86d971808175f33479b3af08ffb1b2b0a5
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-08-03 22:14:39 +0400

    Check

---
+++ MenuAMBackend/Iconditional2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuAMBackend
+{
+    interface Iconditional2
+    {
+        string Price { get; set; }
+        string Weight { get; set; }
+        string Coloria { get; set; }
+
+
+    }
+}

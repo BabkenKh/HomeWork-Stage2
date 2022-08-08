0f5401bf8889bc0328cecb45c492cc5293128f18
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-08-09 03:55:21 +0400

    Generic and add Draft project

---
+++ UseGeneric/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static UseGeneric.Class1;
+
+namespace UseGeneric
+{
+    class Class1
+    {
+    }
+}
---
+++ UseGeneric/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseGeneric
+{
+    class Program
+    {
+        public delegate int  Action();
+        Action action = Sum1;
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+        public static  int Sum1()
+        {
+            Console.WriteLine("fd");
+            return 0;
+        }
+        static T Sum<T>(T a)
+        {
+            return a;
+        }
+    }
+}

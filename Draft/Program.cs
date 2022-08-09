0e97d3686a8e3e3ebf25e177064d74de594e8982
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-08-09 05:31:57 +0400

    Work with Enums and operator "new"

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Draft
 {
-
+    enum DayWeek : byte
+    {
+        Monday = 1,
+        Tuesday ,
+        Wednesday ,
+        Thusday,
+        Friday,
+        Satruday,
+        Sunday,
+    }
     class Program
     {
-
+        
+        
         static public void Swap<T>(ref T x, ref T y)
         {
             T temp = x;
@@ -20,13 +30,15 @@ namespace Draft
         }
         static void Main(string[] args)
         {
-            int x = 5;
-            int y = 10;
-            Console.WriteLine($"X: {x} Y:{y}");
-            Swap(ref x, ref y);
-            Console.WriteLine($"X: {x} Y:{y}");
+            //int x = 5;
+            //int y = 10;
+            //Console.WriteLine($"X: {x} Y:{y}");
+            //Swap(ref x, ref y);
+            //Console.WriteLine($"X: {x} Y:{y}");
+            DayWeek dayWeek = (DayWeek)1;
+            Console.WriteLine(dayWeek);
 
-
+            Console.WriteLine((byte)DayWeek.Monday);
 
             Console.ReadKey();
         }

3177502ab048d1b52f270e9df5c066541b969eff
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-08-09 04:59:13 +0400

    Work with Generic

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -6,41 +6,27 @@ using System.Threading.Tasks;
 
 namespace Draft
 {
-    public class A
-    {
-        public virtual void Print1()
-        {
-            Console.Write("A");
-        }
-        public void Print2()
-        {
-            Console.Write("A");
-        }
-    }
-    public class B : A
-    {
-        public override void Print1()
-        {
-            Console.Write("B");
-        }
-    }
-    public class C : B
-    {
-        new public void Print2()
-        {
-            Console.Write("C");
-        }
-    }
+
     class Program
     {
+
+        static public void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+            
+
+        }
         static void Main(string[] args)
         {
-            var c = new C();
-            A a = c;
+            int x = 5;
+            int y = 10;
+            Console.WriteLine($"X: {x} Y:{y}");
+            Swap(ref x, ref y);
+            Console.WriteLine($"X: {x} Y:{y}");
 
-            a.Print2();
-            a.Print1();
-            c.Print2();
+
 
             Console.ReadKey();
         }

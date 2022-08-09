07c2c5ac1abf0eb551537725c3e3eb010890b56a
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-08-09 18:40:21 +0400

    Work with Delegate

---
+++ WorkWithDelegate/GivePrice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkWithDelegate
+{
+    public delegate void PriceNow(int price);
+    class GivePrice
+    {
+        public PriceNow priceNow { get; set; }
+
+        public void Start()
+        {
+
+            while(true)
+            {
+                int GetPriceFromBank = new Random().Next(100);
+                 
+                priceNow(GetPriceFromBank);
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ WorkWithDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithDelegate
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            GivePrice givePrice = new GivePrice();
+            givePrice.priceNow = ShowPrice;
+
+            givePrice.Start();
+
+            Console.ReadKey();
+        }
+
+      static public void ShowPrice(int price)
+        {
+            Console.WriteLine($"Price : {price}$");
+        }
+    }
+}

d962b4770c4b85a9e083dbd22188218b2e129e45
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-08-11 01:01:32 +0400

    Try to crush

---
+++ DelegateAndEvent/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SalaryCalculate salaryCalculate = new SalaryCalculate();
+
+            salaryCalculate.salary = new Salary(PureSalary);
+            salaryCalculate.salary += GrossSalary;
+            //salaryCalculate.Start();
+
+            var results = salaryCalculate.salary.GetInvocationList().Select(x => (double)x.DynamicInvoke(100000.0));
+            foreach (var result in results)
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        public static double PureSalary(double price)
+        {
+            double pureSalary = price - (price * 0.3);
+
+            return pureSalary;
+        }
+
+        public static double GrossSalary(double price)
+        {
+            
+            return price;
+        }
+
+        public static double Print(double price)
+        {
+        
+            return price;
+        }
+    }
+}
+
+
---
+++ DelegateAndEvent/SalaryCalculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvent
+{
+    public delegate double Salary(double price);
+    class SalaryCalculate
+    {
+        public Salary salary { get; set; }
+
+        //double salary1 = 100000.0;
+    //    public void Start()
+    //    {
+    //        salary(salary1);
+
+    //    }
+    }
+}
